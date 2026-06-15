06c7824eb8d01db8515991c391141f8afd358ee2
DanielPev <daniel.perez@pricetravel.com>
2026-06-15 12:59:11 -0500

    feat: adding pyarrow library

--- parquet_airflow/controllers/jobs_controller.py
+++ parquet_airflow/controllers/jobs_controller.py
@@ -1,3 +1,5 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 class JobsController:
 	__slots__ = ('__job',)
@@ -8,3 +10,14 @@ class JobsController:
 	def run(self):
 		query = self.__job.get('query')
 		print(query)
+	
+	def test_pyarrow(self):
+		data = {
+			'id': [10],
+			'provider_id': [3],
+			'is_afilied': [True],
+			'email': ['Joe.Doh@gmail.com']
+		}
+
+		table = pa.Table.from_pydict(data)
+		pq.write_table(table, 'parquet_test.parquet')
