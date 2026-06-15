4508243ea997eb429a9bf70aaa9288b54ed39f11
DanielPev <daniel.perez@pricetravel.com>
2026-06-15 17:37:02 -0500

    feat: parquet service

--- parquet_airflow/services/parquet_service.py
+++ parquet_airflow/services/parquet_service.py
@@ -1,17 +1,22 @@
 import pyarrow as pa
 import pyarrow.parquet as pq
 from typing import Any
+from parquet_airflow import _ENV
 
 class ParquetService:
 	__slots__ = ('__filepath',)
 
 	def __init__(self):
-		self.__filepath = 'storage/parquets'
+		self.__filepath = _ENV['paths']['parquets']
 
 	def run(self, data: list[dict[str, Any]] | dict[str, Any], filename: str):
-		if isinstance(data, dict):
-			data = [data]
-		dataTable = pa.Table.from_pylist(data)
+		dataNormalized = self.__normalize_data(data)
+		dataTable = pa.Table.from_pylist(dataNormalized)
 
 		filepath = self.__filepath + filename
 		pq.write_table(dataTable, filepath)
+
+	def __normalize_data(self, data: list[dict[str, Any]] | dict[str, Any]) -> list[dict[str, Any]]:
+		if isinstance(data, dict):
+			data = [data]
+		return data
